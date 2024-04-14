238a636b80e585e94d851903a0e8399265ea07fe
ViVarkina <vikavarkina@gmail.com>
2024-04-14 09:38:14 +0300

    14.04

--- templete/scripts/scripts.js
+++ templete/scripts/scripts.js
@@ -69,50 +69,118 @@ const setNextImage=(even, image)=>{
     image.src=imgArr[index]
 
 }
+// const closeButtonModalWindow=()=>{
+//     //кнопка закрытия
+//     const closeButton = document.createElement("div")
+//     closeButton.classList.add("closeButton")
+//     closeButton.innerText = "x"
+//     closeButton.id="closeModalWindowButton"
+//     closeButton.addEventListener("click", ()=>{//передаем в функцию оргумент из другой функции(внутреней)
+//         closeModalWindow(overlay)
+//     })
+// }
+//     return closeButton
+// }
+//
+// const imgModalWindow=(even)=>{
+//     //картинка в модалке
+//     const image = document.createElement("img")
+//     image.src= even.target.src
+//     image.classList.add("modalBodyImg")
+//     return image
+// }
+//
+
+//модальное окно
 const opeModalWindow=(even)=>{
     console.log("клик", even.target)
     //воздаем задний фон модал окна
     const overlay = document.createElement("div")
     overlay.classList.add("modalOverlay")
 
+
     //создаем тело модалки
     const modalBody = document.createElement("div")
     modalBody.classList.add("modalBody")
 
-    //кнопка закрытия
-    const closeButton = document.createElement("div")
-    closeButton.classList.add("closeButton")
-    closeButton.innerText = "x"
-    closeButton.id="closeModalWindowButton"
-    closeButton.addEventListener("click", ()=>{//передаем в функцию оргумент из другой функции(внутреней)
-        closeModalWindow(overlay)
-    })
+    // //кнопка закрытия
+    // const closeButton = document.createElement("div")
+    // closeButton.classList.add("closeButton")
+    // closeButton.innerText = "x"
+    // closeButton.id="closeModalWindowButton"
+    // closeButton.addEventListener("click", ()=>{//передаем в функцию оргумент из другой функции(внутреней)
+    //     closeModalWindow(overlay)
+    // })
 
+    const closeButtonModalWindow=()=>{
+        //кнопка закрытия
+        const closeButton = document.createElement("div")
+        closeButton.classList.add("closeButton")
+        closeButton.innerText = "x"
+        closeButton.id="closeModalWindowButton"
+        closeButton.addEventListener("click", ()=>{//передаем в функцию оргумент из другой функции(внутреней)
+            closeModalWindow(overlay)
+        })
+        return closeButton
+    }
+
+    // //картинка в модалке
+    // const image = document.createElement("img")
+    // image.src= even.target.src
+    // image.classList.add("modalBodyImg")
+    // modalBody.append(image)
+
+
+    //картинка в модалке
+    const imgModalWindow=(even)=>{
     //картинка в модалке
     const image = document.createElement("img")
     image.src= even.target.src
     image.classList.add("modalBodyImg")
-    modalBody.append(image)
+    return image
+}
 
-    //кнопка перелистования
-    const nextImg = document.createElement("div")
-    nextImg.id = "next"
-    nextImg.classList.add("nextImg")
-    nextImg.innerText = ">"
-    nextImg.addEventListener("click",(even)=> setNextImage(even, image))
-    modalBody.append(nextImg)
+
+    //кнопка перелистывания
+    const buttonSwitching=(id,classList,innerText)=>{
+        const switchingImg = document.createElement("div")
+        switchingImg.id = id
+        switchingImg.classList.add(classList)
+        switchingImg.innerText = innerText
+        switchingImg.addEventListener("click",(even)=> setNextImage(even, image))
+        return switchingImg
+    }
+
+    modalBody.append(buttonSwitching("next", "nextImg", ">"))
+    modalBody.append(buttonSwitching("last", "lastImg", "<"))
+
+    // //кнопка перелистования
+    // const nextImg = document.createElement("div")
+    // const lastImg = document.createElement("div")
+    // nextImg.id = "next"
+    // nextImg.classList.add("nextImg")
+    // lastImg.classList.add("lastImg")
+    // nextImg.innerText = ">"
+    // lastImg.innerText = "<"
+    // nextImg.addEventListener("click",(even)=> setNextImage(even, image))
+    // modalBody.append(nextImg)
+
+    //кнопка перелистывания
+    // modalBody.append(nextImg)
+
+    //добавляем картинку в модалку
+    modalBody.append(imgModalWindow())
 
     //добавляем кнопку в окно модал
-    modalBody.append(closeButton)
-
+    modalBody.append(closeButtonModalWindow())
     // засунули тело в фон
     overlay.append(modalBody)
 
     // засунули все в боди
     document.body.append(overlay)
-
 }
 console.log(cards)
+
 
 for (const card of cards) {
     card.addEventListener("click", opeModalWindow)
